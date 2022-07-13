639a3bfc3bd03c1627797093a925b614a34e7291
hyginocit <gnieves@ciandt.com>
2022-07-13 10:06:00 -0300

    0x00

---
+++ 0x00/nomenclatura/Name.java
@@ -0,0 +1,32 @@
+import java.net.URL;
+import java.util.Date;
+import java.util.List;
+
+public class Name {
+
+    public Date modifiedDate;
+
+    public List<Testcase> findAllTestcasesByUser(Name.User user){return null;}
+
+    public List<Account> AccountList;
+
+    public void deleteUser(Name.User user){}
+
+    public URL url;
+
+    public class TableRepresent {};
+    public void saveCar(){};
+
+    public void saveUser(){}; void saveAccount(){}; void saveContract(){};
+
+    public final int DAYS_IN_YEAR = 360;
+    public final int HOURS_IN_WEEK = 168;
+
+    public class User {
+    }
+
+    public class Testcase {
+    }
+
+    public class Account {}
+}
